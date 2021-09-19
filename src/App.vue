f6a86baf85c48ac171c9f3dacb0fe3a4ab8672d1
halim syarif <halimsyarif@192.168.1.6>
2021-09-20 03:48:32 +0700

    create home and addTrip

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
+    <navbar-component></navbar-component>
     <router-view/>
+    <Footer></Footer>
   </div>
 </template>
+
+<script>
+import NavbarComponent from "./components/Navbar.vue"
+import Footer from './components/Footer.vue'
+
+export default {
+  name: "App",
+  components: {
+    NavbarComponent,
+    Footer
+  }
+  
+}
+</script>
 
 <style>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import AddTrip from '../views/AddTrip.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/addtrip',
+    name: 'AddTrip',
+    component: AddTrip,
   }
 ]
 
