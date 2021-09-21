022717a0dad3528a521c121ddf36df03eae3625a
RayendraSabandar <rayendratimotius@gmail.com>
2021-09-21 21:18:09 +0700

    Starting template for register

--- cocktail/src/store/index.js
+++ cocktail/src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import http from '../lib/axios'
+import router from '../router'
 
 Vue.use(Vuex)
 
@@ -9,6 +11,20 @@ export default new Vuex.Store({
   mutations: {
   },
   actions: {
+    async register(context, payload){
+      try {
+        const user = await http({
+          method : 'post',
+          url : 'users/register',
+          data : payload
+        })
+        if(user){
+          router.push({ name : 'Login'})
+        }
+      } catch (err) {
+        console.log(err.response);
+      }
+    }
   },
   modules: {
   }
