b522aa44bfd1cf09862babf8b9556cb9d436e0aa
HojunXie <Hojun.xie2@gmail.com>
2021-09-22 15:57:36 +0700

    initialize tiptap js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
+    <rich-text-editor></rich-text-editor>
   </div>
 </template>
 
 <script>
+import richTextEditor from "../components/richTextEditor.vue"
 export default {
   name: "Home",
   components: {
+    richTextEditor
   },
 };
 </script>
