d530b1cec4aef802db61651c9f453249a3d94899
kautsar_masuara <kautsarmasuara@gmail.com>
2021-09-22 08:36:23 +0800

    interate index.html to Vue

---
+++ covidapp/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"
+      />
+      <title>Page 1</title>
+    </head>
+    <body>
+      <div class="nav-bar mt-2">
+      <nav class="nav-bar-text">
+        <ul>
+          <li><router-link to="/">Home</router-link></li>
+          <li><router-link to="/Search">Search</router-link></li>
+          <li><router-link to="/Map">Map</router-link></li>
+          <li><a href="">Logout </a></li>
+        </ul>
+      </nav>
+    </div>
+    <router-view />
+    </body>
+  </div>
+</template>
+<style scoped>
+body {
+  background-color: rgba(172, 75, 75, 0.678);
+  box-sizing: border-box;
+  margin-top: -20px;
+}
+
+.nav-bar {
+  height: 50px;
+  text-align: center;
+  position: fixed;
+  z-index: 99;
+  border: 2px solid rgb(100, 100, 100);
+  border-radius: 200px;
+  width: 800px;
+  margin-left: 25%;
+  font-size: 20px;
+  background-color: rgba(55, 96, 102, 0.555);
+}
+
+a{
+  text-decoration: none;
+  color: black;
+}
+
+.nav-bar-text {
+  margin-top: 10px;
+}
+
+.nav-bar ul li {
+  list-style-type: none;
+  display: inline;
+  padding: 50px;
+}
+</style>
---
+++ covidapp/src/views/Map.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+        crossorigin="anonymous"
+      />
+      <title>Page 1</title>
+    </head>
+      <!-- Main Feature -->
+      <div id="Main-feature">
+        <div class="container">
+          <!-- Maps -->
+          <div class="container-login row">
+            <div class="search-page row">
+              <div
+                class="page-login col-7"
+                style="background-color: gray"
+              ></div>
+              <div class="login col">
+                <br /><br /><br /><br /><br />
+                <h2>HOSPITAL IN YOUR CITY</h2>
+                <br /><br /><br /><br />
+                <form>
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected>Open this select menu</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </select>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style scoped>
+a {
+    text-decoration: none;
+    color: black;
+}
+.container-login {
+  height: 100vh;
+}
+
+.login-page {
+  height: 80vh;
+  margin-top: 10vh;
+  padding: 20px 150px;
+  box-shadow: 1px 2px 20px -1px;
+}
+
+.login {
+  background-color: rgba(102, 186, 189, 0.836);
+  height: 100%;
+  text-align: center;
+}
+
+.page-login {
+  height: 100%;
+}
+
+.button-login {
+  border: 2px solid blanchedalmond;
+  padding: 2px;
+  width: 250px;
+  background-color: blanchedalmond;
+}
+
+.test {
+  height: 400px;
+  margin-top: 180px;
+  text-align: center;
+  color: #ffff;
+}
+
+.search-page {
+  height: 80vh;
+  margin-top: 10vh;
+  padding: 20px 150px;
+  box-shadow: 1px 2px 20px -1px;
+  background-color: rgb(34, 44, 63);
+  overflow: scroll;
+}
+
+.search {
+  width: 800px;
+  border: 2px solid rgba(219, 211, 211, 0.788);
+  border-radius: 20px;
+}
+
+/* .card-province {
+    height: 100px;
+} */
+
+.text-card {
+  color: #ffff;
+  text-align: center;
+}
+</style>
