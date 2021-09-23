cfb222b8f9ba8c08012a768fd98d8b14d6f965ec
HojunXie <Hojun.xie2@gmail.com>
2021-09-23 12:12:02 +0700

    final, not done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="home">
     <Nav></Nav>
+    <p class="mt-20 text-white text-4xl">Discover Movies</p>
+    <movielist class="mt-5" :movies="movies"></movielist>
   </div>
 </template>
 
 <script>
+import Movielist from '../components/movielist.vue';
 import Nav from "../components/navbar.vue"
+import movielist from "../components/movielist.vue"
 export default {
   name: "Home",
   components: {
     Nav,
+    Movielist,
   },
+  methods: {
+    fetchPopular() {
+      this.$store.dispatch("fetchPopular")
+        .then(res => {
+          this.$store.commit("SET_MOVIES", res.data.results)
+        })
+        .catch(err => console.log(err.response.data))
+    }
+  },
+  computed: {
+    movies() {
+      return this.$store.state.movies
+    }
+  },
+  created() {
+    this.fetchPopular()
+  }
 };
 </script>
